0c8169daca979fa0a9a29e51931887fbdc366d20
galstrom21 <jshepher@rackspace.com>
2012-06-04 22:14:32 -0500

    adding monit_procmon calls to api and registry

--- recipes/registry.rb
+++ recipes/registry.rb
@@ -189,3 +189,10 @@ template "/etc/glance/glance-registry-paste.ini" do
   )
   notifies :restart, resources(:service => "glance-registry"), :immediately
 end
+
+include_recipe "monit::server"
+monit_procmon "glance-registry" do
+  process_name "glance-registry"
+  start_cmd platform_options["monit_commands"]["glance-registry"]["start"]
+  stop_cmd platform_options["monit_commands"]["glance-registry"]["stop"]
+end
