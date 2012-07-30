ac14b17064b0e8a8e20b1f7055b1f31805d802f6
galstrom21 <jshepher@rackspace.com>
2012-07-30 15:51:15 -0500

    Updating copyright string

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-maintainer        "Rackspace Hosting, Inc."
+maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
