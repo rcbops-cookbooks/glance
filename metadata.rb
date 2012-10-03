ae9c8eb98ad7a995ac36fa57e8fee5e99d56fd05
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-10-03 10:57:57 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.9"
+version           "1.0.10"
 recipe            "glance::api", "Installs packages required for a glance api server"
 recipe            "glance::registry", "Installs packages required for a glance registry server"
 
