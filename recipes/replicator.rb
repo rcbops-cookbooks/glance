599613551b3eea21e542551656f44ee4e5be43f4
Matt Thompson <mattt@defunct.ca>
2013-03-01 13:15:07 -0600

    Cleanup search

--- recipes/replicator.rb
+++ recipes/replicator.rb
@@ -17,18 +17,21 @@
 # limitations under the License.
 #
 
-if Chef::Config[:solo]
-  Chef::Application.fatal! "This recipe uses search. Chef Solo does not support search."
-end
-
 glance_servers = get_realserver_endpoints("glance-api", "glance", "api").map { |g| g["host"] }
 me = get_ip_for_net(node["glance"]["services"]["api"]["network"], node)
 glance_servers.delete(me)
 # This recipe is only called when we have 2 glance-api servers, so there should
 # only be one left once we remove ourself.
 other_glance_server_ip = glance_servers[0]
-other_glance_server_node = search(:node, "chef_environment:#{node.chef_environment} AND network_*_addresses:#{other_glance_server_ip}")[0]
-other_glance_server_port = other_glance_server_node["openssh"]["server"]["port"]
+
+if Chef::Config[:solo]
+  Chef::Application.fatal! "This recipe uses search. Chef Solo does not support search."
+  other_glance_server_port = "22"
+else
+  other_glance_server_node = search(:node, "chef_environment:#{node.chef_environment} AND network_*_addresses:#{other_glance_server_ip}")[0]
+  other_glance_server_port = other_glance_server_node["openssh"]["server"]["port"]
+end
+
 glance_endpoint = get_access_endpoint("glance-api", "glance", "api")
 
 dsh_group "glance" do
