cc6e39c3f1b0d9d31a20c6e0307fedab3ea637fe
Joe Breu <breu@breu.org>
2013-10-08 18:04:11 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.19"
+version           "4.2.0"
 recipe            "glance::setup", "Handles glance keystone registration and database creation"
 recipe            "glance::api", "Installs packages required for a glance api server"
 recipe            "glance::registry", "Installs packages required for a glance registry server"
